70234afa8465f8fb1a4e4bb82aea3a66097a75b9
JudiPARK0416 <p.eunjung.0425@gmail.com>
2025-04-10 14:20:57 +1000

    lets see if the import line works like this

--- src/AppProfile.jsx
+++ src/AppProfile.jsx
@@ -1,25 +1,11 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-
-
+import Profile from "./components/profile.jsx";
 // https://transform.tools/html-to-jsx
 
 function AppProfile() {
-  const name = "Judy";
-  const list = ["milk", "bread", "egg"];
-
   return (
     <>
-      <h1>{`Hello! ${name}`}</h1>
-      <h2>Bye!</h2>
-      <p>{name}</p>
-      <ul>
-        {list.map((item) => (
-          <li>{item}</li>
-        ))}
-      </ul>
+      <Profile />
     </>
   );
 }
