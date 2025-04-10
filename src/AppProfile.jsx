5b550b474bcda858ee849aeaf5d06079696e5f23
JudiPARK0416 <p.eunjung.0425@gmail.com>
2025-04-10 15:42:39 +1000

    componentising + props rendering

--- src/AppProfile.jsx
+++ src/AppProfile.jsx
@@ -1,11 +1,30 @@
 import "./App.css";
 import Profile from "./components/Profile.jsx";
-// https://transform.tools/html-to-jsx
+import Avatar from "./components/Avatar";
 
 function AppProfile() {
   return (
     <>
-      <Profile />
+      <Avatar
+        image="https://images.unsplash.com/photo-1734630630491-458df4f38213?w=1400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8"
+        isNew={true}
+      />
+      <Profile
+        image="https://images.unsplash.com/photo-1734630630491-458df4f38213?w=1400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8"
+        name="Judy Park"
+        title="Frontend Developer"
+        isNew={true}
+      />
+      <Profile
+        image="https://images.unsplash.com/photo-1734630630491-458df4f38213?w=1400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8"
+        name="Judy PARK"
+        title="Frontend Developer"
+      />
+      <Profile
+        image="https://images.unsplash.com/photo-1734630630491-458df4f38213?w=1400&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHwzfHx8ZW58MHx8fHx8"
+        name="Judy Park"
+        title="Frontend Developer"
+      />
     </>
   );
 }
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,5 +1,11 @@
-function Profile() {
-  return <h1>Profile</h1>;
-}
+import Avatar from "./Avatar";
 
-export default Profile;
+export default function Profile({ image, name, title, isNew }) {
+  return (
+    <div className="profile">
+      <Avatar image={image} isNew={isNew} />
+      <h1>{name}</h1>
+      <p>{title}</p>
+    </div>
+  );
+}
